09d33b4b0b828e58cb0e0eaa930332e560f4db21
Aktilek <103207943+SelVord@users.noreply.github.com>
2023-12-21 21:51:15 +0600

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.projectoop {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.projectoop to javafx.fxml;
-    exports com.example.projectoop;
-}
